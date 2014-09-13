b66cacbe0151d6b11dd5820f4af57a43c8d362bd
Jack Coulter <jscinoz@jscinoz.so>
2014-09-13 17:32:07 +1000

    Entry point is src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-module.exports = require("./main");
+"use strict";
+
+var World = require("./World"),
+    Circle = require("./Circle"),
+    Genotype = require("./Genotype");
+
+function init() {
+    var canvas = document.querySelector("canvas"),
+        ctx = canvas.getContext("2d"),
+        world = new World(0xdeadbeefcafe);
+
+    canvas.width = world.width;
+    canvas.height = world.height;
+
+    world.start(ctx);
+};
+
+if (typeof window !== "undefined") {
+    // We're in a browser!
+    window.addEventListener("DOMContentLoaded", init);
+}
